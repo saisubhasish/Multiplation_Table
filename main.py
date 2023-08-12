41bc17398f950163be1c2071b0c3cc60ae4e529e
saisubhasish <saisubhasishrout777@gmail.com>
2023-08-12 19:20:27 +0530

    Added data_ingestion component

---
+++ main.py
@@ -0,0 +1,21 @@
+
+import os, sys
+
+from multiplicationTable.entity import config_entity
+from multiplicationTable.components.data_ingestion import DataIngestion
+
+from multiplicationTable.exception import MultiplicationException
+
+
+
+try:
+    training_pipeline_config = config_entity.TrainingPipelineConfig()
+
+    #data ingestion         
+    data_ingestion_config  = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+    print(data_ingestion_config.to_dict())
+    data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
+    data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+
+except Exception as e:
+    raise MultiplicationException(error_message=e, error_detail=sys)
--- multiplicationTable/entity/config_entity.py
+++ multiplicationTable/entity/config_entity.py
@@ -21,8 +21,8 @@ class TrainingPipelineConfig:
 class DataIngestionConfig:
     def __init__(self, training_pipeline_config:TrainingPipelineConfig):
         try:
-            self.database_name="HealthCare"
-            self.collection_name="Thyroid"
+            self.database_name="MultiplicationTable"
+            self.collection_name="Multiplication"
             self.data_ingestion_dir = os.path.join(training_pipeline_config.artifact_dir , "data_ingestion")
             self.feature_store_file_path = os.path.join(self.data_ingestion_dir,"feature_store",FILE_NAME)
             self.train_file_path = os.path.join(self.data_ingestion_dir,"dataset",TRAIN_FILE_NAME)
