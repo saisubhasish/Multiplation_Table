cee56e7fd548f129daaffcdafe5d6b6f1ccd5a8f
saisubhasish <saisubhasishrout777@gmail.com>
2023-08-15 19:49:56 +0530

    CI-CD updated

--- app.py
+++ app.py
@@ -52,4 +52,5 @@ def predict_api():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=8080)
+    # app.run(host="0.0.0.0", port = 8080, debug=True)
+    app.run(host='0.0.0.0', port=8080)
